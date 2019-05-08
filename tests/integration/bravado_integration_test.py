ba10a4b02543c37d1c60a05205cb2da351ac6fca
Stephan Jaensch <sjaensch@yelp.com>
2019-05-08 16:01:46 +0200

    Omit coverage check for bravado_integration_test.py as a last resort to fixing the Azure Pipeline on Windows issues

--- tests/integration/bravado_integration_test.py
+++ tests/integration/bravado_integration_test.py
@@ -27,7 +27,7 @@ class TestServerBravadoAsyncioClient(IntegrationTestsBaseClass):
             'method': 'GET',
             'headers': {'byte-header': b'1'},
             'url': '{server_address}/1'.format(server_address=swagger_http_server),
-            'params': {'connect_timeout': 0.1, 'timeout': 1},
+            'params': {},
         }).result(timeout=1)
 
         assert response.text == self.encode_expected_response(ROUTE_1_RESPONSE)
