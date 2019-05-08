922d17ca6d9babb9c8acf608499ddf5719458e4d
Stephan Jaensch <sjaensch@yelp.com>
2019-05-08 15:02:31 +0200

    Use newest bravado version for running tests so integration tests work on Windows, additional fixes on Windows

--- tests/future_adapter_test.py
+++ tests/future_adapter_test.py
@@ -1,5 +1,6 @@
 import asyncio
 import concurrent.futures
+import time
 
 import mock
 import pytest
@@ -25,8 +26,24 @@ def mock_wait_for():
         yield _mock
 
 
+def mock_result_with_sleep(response):
+    """Return a function that can be used as side effect for mock_future.result, adding in a delay."""
+    def _side_effect(timeout=None):
+        time.sleep(0.1)
+        return response
+    return _side_effect
+
+
+def mock_wait_for_result_with_sleep(response):
+    """Return a function that can be used as side effect for mock_wait_for, adding in a delay."""
+    def _side_effect(future, timeout=None):
+        time.sleep(0.1)
+        return asyncio.coroutine(lambda: response)()
+    return _side_effect
+
+
 def test_future_adapter(mock_future, mock_response):
-    mock_future.result.return_value = mock_response
+    mock_future.result.side_effect = mock_result_with_sleep(mock_response)
 
     future_adapter = FutureAdapter(mock_future)
     result = future_adapter.result(timeout=5)
@@ -35,8 +52,6 @@ def test_future_adapter(mock_future, mock_response):
     assert result.response is mock_response
     assert 0 < result.remaining_timeout < 5
 
-    mock_future.result.assert_called_once_with(5)
-
 
 def test_future_adapter_timeout_error_class():
     """Let's make sure refactors never break timeout errors"""
@@ -44,7 +59,7 @@ def test_future_adapter_timeout_error_class():
 
 
 def test_asyncio_future_adapter(mock_future, mock_wait_for, mock_response, event_loop):
-    mock_wait_for.return_value = asyncio.coroutine(lambda: mock_response)()
+    mock_wait_for.side_effect = mock_wait_for_result_with_sleep(mock_response)
 
     future_adapter = AsyncioFutureAdapter(mock_future)
     result = event_loop.run_until_complete(future_adapter.result(timeout=5))
