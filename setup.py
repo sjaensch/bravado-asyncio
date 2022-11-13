fd0a060f383dc3f371f73dd2b181be63a45ce118
Stephan Jaensch <sj@sjaensch.org>
2022-11-13 23:48:04 +0100

    Also remove Python 3.7 support

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
